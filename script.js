3ef145bf2bf03ed045ee48320744537b575f9dfb
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-01 11:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+const list = document.getElementById('infi-list');
+const additionalItems = [
+  'Item 1',
+  'Item 2'
+];
 
+function appendListItem(text) {
+  const li = document.createElement('li');
+  li.textContent = text;
+  list.appendChild(li);
+}
+
+function loadMoreItems() {
+  additionalItems.forEach((item) => {
+    appendListItem(item);
+  });
+}
+
+list.addEventListener('scroll', () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    loadMoreItems();
+  }
+});
